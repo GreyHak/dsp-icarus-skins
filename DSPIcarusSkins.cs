d1d188042296fd73a7eaeae06fbfed5346f39b16
GreyHak <479851+GreyHak@users.noreply.github.com>
2021-04-15 20:16:12 -0700

    Initial Icarus skin functionality

--- DSPIcarusSkins.cs
+++ DSPIcarusSkins.cs
@@ -15,6 +15,8 @@ using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
 using UnityEngine;
+using System.Reflection;
+using System.IO;
 
 namespace DSPIcarusSkins
 {
@@ -29,6 +31,20 @@ namespace DSPIcarusSkins
         new internal static BepInEx.Configuration.ConfigFile Config;
         Harmony harmony;
 
+        public static BepInEx.Configuration.ConfigEntry<UInt16> configSkinSelection;
+        public static BepInEx.Configuration.ConfigEntry<string> configSkinPath;
+
+        public static readonly string[] resourceNames = {
+                "",
+                "DSPIcarusSkins.Built_In_Skins.camoDark.png",
+                "DSPIcarusSkins.Built_In_Skins.camoLight.png",
+                "DSPIcarusSkins.Built_In_Skins.camoRWB.png",
+                "DSPIcarusSkins.Built_In_Skins.blue.png",
+                "DSPIcarusSkins.Built_In_Skins.color.png",
+                "DSPIcarusSkins.Built_In_Skins.bluegold.png",
+                "DSPIcarusSkins.Built_In_Skins.redgold.png",
+            };
+
         public void Awake()
         {
             Logger = base.Logger;  // "C:\Program Files (x86)\Steam\steamapps\common\Dyson Sphere Program\BepInEx\LogOutput.log"
@@ -36,11 +52,99 @@ namespace DSPIcarusSkins
 
             harmony = new Harmony(pluginGuid);
             harmony.PatchAll(typeof(DSPIcarusSkins));
+
+            /*string[] resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            Logger.LogDebug("Resources:");
+            foreach (string resourceName in resourceNames)
+            {
+                Logger.LogDebug($"Resource Name \"{resourceName}\".");
+            }*/
+
+            configSkinSelection = Config.Bind<UInt16>("Skin", "Selection", 2, new BepInEx.Configuration.ConfigDescription("0:Path Provided;  1:Dark Camo;  2:Light Camo;  3:Red/White/Blue Camo;  4:Blue;  5:Color;  6:Blue/gold;  7:Red/gold", new BepInEx.Configuration.AcceptableValueRange<UInt16>(0, 8)));
+            configSkinPath = Config.Bind<string>("Skin", "Path", "", "Path to the 2048 x 2048 skin image.  This setting is only used if Selection is set to 0.");
+            OnConfigChanged(null, null);
+            Config.ConfigReloaded += OnConfigChanged;
+            Config.SettingChanged += OnConfigChanged;
         }
 
-        [HarmonyPfix, HarmonyPatch(typeof(CLASS), "FUNCTION")]
-        public static void CLASS_FUNCTION_Pfix()
+        [HarmonyPostfix, HarmonyPatch(typeof(PlayerAnimator), "Start")]
+        public static void PlayerAnimator_Start_Postfix()
         {
+            Config.Reload();
+            OnConfigChanged(null, null);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(GameMain), "Resume")]
+        public static void GameMain_Resume_Postfix()
+        {
+            Config.Reload();
+            OnConfigChanged(null, null);
+        }
+
+        public static void OnConfigChanged(object sender, EventArgs e)
+        {
+            bool icarusArmorFlag = false;
+            bool icarusSkeletonFlag = false;
+            Renderer[] componentsInChildren = GameMain.mainPlayer.animator.GetComponentsInChildren<Renderer>(true);
+            foreach (Renderer renderer in componentsInChildren)
+            {
+                var mat = renderer.sharedMaterial;
+                if (mat != null)
+                {
+                    if (!icarusArmorFlag && mat.name.StartsWith("icarus-armor"))
+                    {
+                        Texture2D newIcarusArmorTexture = new Texture2D(2048, 2048);
+                        if (configSkinSelection.Value == 0 || configSkinSelection.Value >= resourceNames.Length)
+                        {
+                            string icarusArmorFilePath = configSkinPath.Value;
+                            if (System.IO.File.Exists(icarusArmorFilePath))
+                            {
+                                byte[] fileData = System.IO.File.ReadAllBytes(icarusArmorFilePath);
+                                if (newIcarusArmorTexture.LoadImage(fileData))
+                                {
+                                    Logger.LogInfo($"Successfully loaded custom icarus armour skin {icarusArmorFilePath}");
+                                }
+                            }
+                        }
+                        else
+                        {
+                            string resourceName = resourceNames[configSkinSelection.Value];
+                            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+                            if (stream != null)
+                            {
+                                byte[] fileData = new byte[stream.Length];
+                                stream.Read(fileData, 0, (int)stream.Length);
+                                if (newIcarusArmorTexture.LoadImage(fileData))
+                                {
+                                    Logger.LogInfo($"Successfully loaded built-in icarus armour skin {resourceName}");
+                                }
+                            }
+                        }
+
+                        for (int x = 0; x < newIcarusArmorTexture.width; x++)
+                        {
+                            for (int y = 0; y < newIcarusArmorTexture.height; y++)
+                            {
+                                Color pixel = newIcarusArmorTexture.GetPixel(x, y);
+                                newIcarusArmorTexture.SetPixel(x, y, new Color(pixel.r, pixel.g, pixel.b, 0));
+                            }
+                        }
+                        newIcarusArmorTexture.Apply();
+
+                        mat.mainTexture = newIcarusArmorTexture;
+
+                        icarusArmorFlag = true;
+                        if (icarusArmorFlag && icarusSkeletonFlag)
+                            break;
+                    }
+                    else if (!icarusSkeletonFlag && mat.name.StartsWith("icarus-skeleton"))
+                    {
+                        icarusSkeletonFlag = true;
+                        if (icarusArmorFlag && icarusSkeletonFlag)
+                            break;
+                    }
+                }
+            }
         }
     }
 }
--- Libraries/_Copy_DLLs_Here.bat
+++ Libraries/_Copy_DLLs_Here.bat
@@ -4,6 +4,7 @@ COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\BepInEx\co
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\Assembly-CSharp.dll" .
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\UnityEngine.UI.dll" .
 COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\UnityEngine.CoreModule.dll" .
+COPY "%ProgramFiles(x86)%\Steam\steamapps\common\Dyson Sphere Program\DSPGAME_Data\Managed\UnityEngine.ImageConversionModule.dll" .
 
 ECHO UnityEngine.dll from this post https://discord.com/channels/806549677209944084/806556447885623336/809242935073832970
 ECHO Direct link: https://cdn.discordapp.com/attachments/806556447885623336/809242933433598002/UnityEngine.refstub.dll
